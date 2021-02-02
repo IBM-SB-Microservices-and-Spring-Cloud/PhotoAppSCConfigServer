1b7a49fbd02ff29c6f61b59fec50291cf5e9812b
Abdul426 <munnaf426@gmail.com>
2021-02-02 15:20:55 +0530

    first commit

---
+++ src/test/java/com/photoapp/springcloudconfigserver/PhotoAppScConfigServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.photoapp.springcloudconfigserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PhotoAppScConfigServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
